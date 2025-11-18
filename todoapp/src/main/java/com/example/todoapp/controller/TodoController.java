66f3ee3826281c0f2c3010e4243a76010e4fa659
sukyoung14 <ssukyoung14@gmail.com>
2025-11-18 10:40:20 +0900

    Add todoapp project

---
+++ todoapp/src/main/java/com/example/todoapp/controller/TodoController.java
@@ -0,0 +1,37 @@
+package com.example.todoapp.controller;
+
+import com.example.todoapp.dto.TodoDto;
+import com.example.todoapp.repository.TodoRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TodoController {
+    private final TodoRepository todoRepository = new TodoRepository();
+
+    @GetMapping("/todos")
+    public String todos(){
+        //TodoRepository todoRepository = new TodoRepository();  //이전에 만들었던 Repository와 다른 객체를 사용하면 안됨
+
+        
+        return "todos";
+    }
+
+    @GetMapping("/todos/new")
+    public String newTodos(){
+        return "new";
+    }
+
+    @GetMapping("/todos/create")
+    public String create(@RequestParam String title, @RequestParam String content, Model model){
+        TodoDto todoDto = new TodoDto(null, title, content, false);
+        //TodoRepository todoRepository = new TodoRepository(); //전역으로 변경
+        TodoDto todo = todoRepository.Save(todoDto);
+        model.addAttribute("todo", todo);
+        return "create";
+    }
+
+
+}
---
+++ todoapp/src/main/java/com/example/todoapp/repository/TodoRepository.java
@@ -0,0 +1,17 @@
+package com.example.todoapp.repository;
+
+import com.example.todoapp.dto.TodoDto;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TodoRepository {
+    private final Map<Long, TodoDto> storage = new ConcurrentHashMap<>();
+    private Long nextId = 1l;
+
+    public TodoDto Save(TodoDto todo){
+        todo.setId(nextId++);
+        storage.put(todo.getId(), todo);
+        return todo;
+    }
+}
