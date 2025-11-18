fc1c01b6358b66ac9f0a891aedc2f21c42915c8d
sukyoung14 <ssukyoung14@gmail.com>
2025-11-18 11:42:18 +0900

    의존성 주입

--- todoapp/src/main/java/com/example/todoapp/repository/TodoRepository.java
+++ todoapp/src/main/java/com/example/todoapp/repository/TodoRepository.java
@@ -1,12 +1,14 @@
 package com.example.todoapp.repository;
 
 import com.example.todoapp.dto.TodoDto;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Repository
 public class TodoRepository {
     private final Map<Long, TodoDto> storage = new ConcurrentHashMap<>();
     private Long nextId = 1l;
@@ -18,5 +20,8 @@ public class TodoRepository {
     }
     public List<TodoDto> findAll(){
         return new ArrayList<TodoDto>(storage.values());
+    }
+    public TodoDto findById(Long id) {
+        return storage.get(id);
     }
 }
