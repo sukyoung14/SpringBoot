b893cf984e14d3029cdf0d06dddda7fb7de025d3
sukyoung14 <ssukyoung14@gmail.com>
2025-11-28 11:15:05 +0900

    사용자 정보 출력

--- security/src/main/java/com/example/securitydemo/controller/Authcontroller.java
+++ security/src/main/java/com/example/securitydemo/controller/Authcontroller.java
@@ -1,6 +1,9 @@
 package com.example.securitydemo.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,7 +15,12 @@ public class Authcontroller {
     }
 
     @GetMapping("dashboard")
-    public String dashboard(){
+    public String dashboard(@AuthenticationPrincipal UserDetails userDetails,  Model model){
+        if (userDetails != null) {
+            model.addAttribute("userName", userDetails.getUsername());
+            model.addAttribute("authorities", userDetails.getAuthorities());
+            model.addAttribute("password", userDetails.getPassword());
+        }
         return "dashboard";
     }
 
