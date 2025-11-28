2e5480ef3add41abd8dff4fa9968dda3fae98688
sukyoung14 <ssukyoung14@gmail.com>
2025-11-28 10:30:24 +0900

    샘플 user DATA

--- security/src/main/java/com/example/securitydemo/config/SecurityConfig.java
+++ security/src/main/java/com/example/securitydemo/config/SecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,16 +19,37 @@ public class SecurityConfig {
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/","/info").permitAll()   // 두개의 라우팅은 열고
+                    .requestMatchers("/","/info", "/login").permitAll()   // 라우팅은 열고
                     .anyRequest().authenticated()       // 니머지 모두는 로그인 필요
             )
             .formLogin(form -> form
+                    .loginPage("/login")    // 커스텀 로그인 페이지 주소
+                    .defaultSuccessUrl("/dashboard", true)  // 로그인 성공시 이동할 페이지
+                    .failureUrl("/login")   //로그인 실패시 이동할 페이지
                     .permitAll()
             )
             .logout(logout -> logout
+                    .logoutSuccessUrl("/login") //로그아웃 성공시 이동할 페이지
                     .permitAll()
             );
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}1234")
+                .roles("USER")
+                .build();
+        // new User("user", "{noop}1234", "USER")
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}admin")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user,admin);
     }
 }
--- security/src/main/java/com/example/securitydemo/controller/HomeController.java
+++ security/src/main/java/com/example/securitydemo/controller/HomeController.java
@@ -15,4 +15,6 @@ public class HomeController {
     public String info(){
         return "info";
     }
+
+
 }
