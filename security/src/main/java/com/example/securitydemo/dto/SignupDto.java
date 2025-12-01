3089307789d88c49bb03461e9bdf72ae86b273d2
sukyoung14 <ssukyoung14@gmail.com>
2025-12-01 09:25:01 +0900

    Validation

---
+++ security/src/main/java/com/example/securitydemo/dto/SignupDto.java
@@ -0,0 +1,26 @@
+package com.example.securitydemo.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignupDto {
+    @NotBlank(message = "사용자명을 입력해주세요")
+    @Size(min=3, max=20, message="사용자명은 3~20자입니다.")
+    private String username;
+    @Size(min=3, max=20, message="비밀번호는 3~20자입니다.")
+    @NotBlank(message = "비밀번호를 입력해주세요")
+    private String password;
+    @NotBlank(message = "비밀번호를 확인을 입력해주세요")
+    @Size(min=3, max=20, message="비밀번호는 3~20자입니다.")
+    private String passwordConfirm;
+    
+    // Pattern => 정규표현식 사용가능
+    @NotBlank(message = "이메일을 입력해주세요")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    private String email;
+}
